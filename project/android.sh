de036a01bfd7131b34a4366b310d386a4dfbe5dc
vincent-schirmann <v@narenjo.com>
2020-08-10 12:06:42 +0200

    rebuild android

--- project/android.sh
+++ project/android.sh
@@ -1,8 +1,12 @@
-rm -rf "obj"
-rm -rf "all_objs"
-echo "Compiling for armv6"
-haxelib run hxcpp Build.xml -Dandroid
-echo "Compiling for armv7"
-haxelib run hxcpp Build.xml -Dandroid -DHXCPP_ARMV7
-rm -rf "obj"
-rm -rf "all_objs"
+# rm -rf "obj"
+# rm -rf "all_objs"
+# echo "Compiling for armv6"
+# haxelib run hxcpp Build.xml -Dandroid
+# echo "Compiling for armv7"
+# haxelib run hxcpp Build.xml -Dandroid -DHXCPP_ARMV7
+# echo "Compiling for arm64"
+# haxelib run hxcpp Build.xml -Dandroid -DHXCPP_ARMV7
+# rm -rf "obj"
+# rm -rf "all_objs"
+
+lime rebuild . android
